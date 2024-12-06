96531a4420bafc34393a2b9c5af4b821744ec70d
matzutake <viskas21rus@gmail.com>
2024-12-06 16:27:27 +0300

    feat: Добавил роутер, прикрутил всё, создал home страницу, почистил проект

---
+++ src/router/index.ts
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+
+// Routes
+import Home from '../views/Home.vue'
+
+const routes: Array<RouteRecordRaw> = [
+	{
+		path: '/',
+		name: 'Home',
+		component: Home,
+	},
+]
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes,
+})
+
+export default router
